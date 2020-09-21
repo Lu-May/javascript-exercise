71861702fdca3814df4582267a639bd175413acb
unknown <1054998345@qq.com>
2020-09-21 15:55:59 +0800

    [luyuehuan] feat:filter number

--- 2-array/filter/index.js
+++ 2-array/filter/index.js
@@ -1,5 +1,9 @@
 function chooseMultiplesOfThree(collection) {
   // TODO 1: 在这里写实现代码
+  const result = collection.filter(function filterNumber(number) {
+    return number % 3 === 0;
+  });
+  return result;
 }
 
 function chooseNoRepeatNumber(collection) {
