bfe2d1204e4f586ae49f356c2955f86d0f4b61cc
unknown <1054998345@qq.com>
2020-09-21 21:41:35 +0800

    [luyuehuan] feat:get the total values

--- 3-object/values/index.js
+++ 3-object/values/index.js
@@ -1,3 +1,9 @@
 export default function countTypesNumber(source) {
   // TODO 6: 在这里写实现代码
+  const valuearr = Object.values(source);
+  let result = 0;
+  for (let index = 0; index < valuearr.length; index += 1) {
+    result += Number(valuearr[index]);
+  }
+  return result;
 }
