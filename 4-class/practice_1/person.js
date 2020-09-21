a630bdf300adb3a258496bc9e5acfa450a267b9f
unknown <1054998345@qq.com>
2020-09-21 22:01:54 +0800

    [luyuehuan] feat:practice-1 of 4-class

--- 4-class/practice_1/person.js
+++ 4-class/practice_1/person.js
@@ -1,1 +1,8 @@
 // TODO 9: 在这里写实现代码
+export default function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.introduce = function returnString() {
+    return `My name is ${this.name}. I am ${this.age} years old.`;
+  };
+}
