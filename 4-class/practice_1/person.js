40e21502f9b3de7de756b0ec7335ba750eb7118b
Lu-May <1054998345@qq.com>
2020-10-08 17:48:47 +0800

    [luyuehuan] feat:impletment 4-class practice_2

--- 4-class/practice_1/person.js
+++ 4-class/practice_1/person.js
@@ -1,8 +1,11 @@
 // TODO 9: 在这里写实现代码
-export default function Person(name, age) {
-  this.name = name;
-  this.age = age;
-  this.introduce = function returnString() {
+export default class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  introduce() {
     return `My name is ${this.name}. I am ${this.age} years old.`;
-  };
+  }
 }
