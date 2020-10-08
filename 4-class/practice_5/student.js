0f9aae9f97924e3434938ff33408d437184b776a
Lu-May <1054998345@qq.com>
2020-10-08 18:06:18 +0800

    [luyuehuan] feat:impletment 4-class practice_5

--- 4-class/practice_5/student.js
+++ 4-class/practice_5/student.js
@@ -1,1 +1,16 @@
 // TODO 19: 在这里写实现代码
+import Person from './person';
+
+export default class Student extends Person {
+  constructor(name, age, klass) {
+    super(name, age);
+    this.klass = klass;
+  }
+
+  introduce() {
+    if (this.klass === undefined) {
+      return `${super.introduce()} I am a Worker. I have a job.`;
+    }
+    return `${super.introduce()} I am a Student. I am at Class ${this.klass}.`;
+  }
+}
