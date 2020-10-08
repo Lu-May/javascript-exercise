8292b4812b963995897b47c1aabfa75a4808b97d
Lu-May <1054998345@qq.com>
2020-10-08 21:55:14 +0800

    [luyuehuan] feat:impletmrnt 5-async 4-async

--- 5-async/4-async/index.js
+++ 5-async/4-async/index.js
@@ -1,6 +1,13 @@
 async function fetchData(url) {
   // <-- start
   // TODO 24: 通过await/async实现异步请求
+  try {
+    const requestResult = await fetch(url);
+    const data = await requestResult.json();
+    document.writeln(data.name);
+  } catch (error) {
+    console.error(error);
+  }
   // end -->
 }
 
